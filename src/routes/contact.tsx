fa071387eed923acb06ca04ebec5d6786a0518c7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-03-26 15:15:06 +0100

    feat: implement contact form with validation and UI components

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,4 +1,6 @@
+import { FormContact } from '@/components/business/form-contact'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
 import { Link, createFileRoute } from '@tanstack/react-router'
 import { FileInputIcon } from 'lucide-react'
 
@@ -12,6 +14,11 @@ function Contact() {
       <div>
         If you want to contact us please fill the form <FileInputIcon className="inline" /> below :
       </div>
+      <Card>
+        <CardContent>
+          <FormContact />
+        </CardContent>
+      </Card>
       <Link to="/">
         <Button variant="destructive">Go to home</Button>
       </Link>
