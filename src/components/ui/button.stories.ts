0a85f96d54c0ad46f7460a70d77fab948ab72d3a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-03-28 15:14:03 +0100

    chore: clean Storybook samples

--- src/components/ui/button.stories.ts
+++ src/components/ui/button.stories.ts
@@ -21,6 +21,7 @@ const meta = {
 } satisfies Meta<typeof Button>
 
 export default meta
+
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
