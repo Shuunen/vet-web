86a406dd51055afa84451d8554df9b7779d4c87b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-03-31 09:04:33 +0200

    refactor: rename styling utility

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { Controller, type ControllerProps, type FieldPath, type FieldValues, FormProvider, useFormContext, useFormState } from 'react-hook-form'
 
 import { Label } from '@/components/ui/label'
-import { cn } from '@/lib/utils'
+import { cn } from '@/utils/styling.utils'
 
 const Form = FormProvider
 
