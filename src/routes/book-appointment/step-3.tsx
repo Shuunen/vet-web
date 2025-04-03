06f4c632c214b2a3bf3674032716437c0b1f67a0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-04-03 15:11:12 +0200

    feat: refactor SummaryPage to dynamically display base and complementary data

--- src/routes/book-appointment/step-3.tsx
+++ src/routes/book-appointment/step-3.tsx
@@ -3,53 +3,30 @@ import { useBookAppointmentStore } from '@/utils/book-appointment.store'
 import { Link, createFileRoute } from '@tanstack/react-router'
 import { ArrowLeftIcon, Calendar1Icon } from 'lucide-react'
 
-// eslint-disable-next-line max-lines-per-function
 function SummaryPage() {
   const { data, setCurrentStep } = useBookAppointmentStore()
   // eslint-disable-next-line no-magic-numbers
   setCurrentStep(2)
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="grid grid-cols-2 gap-2 text-sm">
-        <div className="text-gray-600">Pet Identifier:</div>
-        <div>{data.baseData.identifier}</div>
+    <div className="flex flex-col gap-2">
+      <h3 className="col-span-2 font-bold text-xl">Base data</h3>
+      {Object.entries(data.baseData).map(([key, value]) => (
+        <div className="grid grid-cols-2" key={key}>
+          <div className="text-gray-500">{key}</div>
+          <div>{value}</div>
+        </div>
+      ))}
 
-        <div className="text-gray-600">Name:</div>
-        <div>{data.baseData.name}</div>
+      <h3 className="col-span-2 font-bold text-xl">Complementary data</h3>
+      {Object.entries(data.complementaryData).map(([key, value]) => (
+        <div className="grid grid-cols-2" key={key}>
+          <div className="text-gray-500">{key}</div>
+          <div>{value}</div>
+        </div>
+      ))}
 
-        <div className="text-gray-600">Age:</div>
-        <div>{data.baseData.age}</div>
-
-        <div className="text-gray-600">Type:</div>
-        <div className="capitalize">{data.baseData.type}</div>
-
-        {'indoorOutdoor' in data.complementaryData ? (
-          <>
-            <div className="text-gray-600">Environment:</div>
-            <div className="capitalize">{data.complementaryData.indoorOutdoor}</div>
-
-            <div className="text-gray-600">Last Flea Treatment:</div>
-            <div>{data.complementaryData.lastFleaTreatment}</div>
-
-            <div className="text-gray-600">Vaccination Status:</div>
-            <div>{data.complementaryData.vaccinationStatus}</div>
-          </>
-        ) : (
-          <>
-            <div className="text-gray-600">Breed:</div>
-            <div>{data.complementaryData.breed}</div>
-
-            <div className="text-gray-600">Weight:</div>
-            <div>{data.complementaryData.weight} kg</div>
-
-            <div className="text-gray-600">Exercise Routine:</div>
-            <div>{data.complementaryData.exerciseRoutine}</div>
-          </>
-        )}
-      </div>
-
-      <div className="flex gap-18 mt-4">
+      <div className="flex gap-18 mt-6">
         <Link to={`/book-appointment/${data.baseData.type}/step-2`}>
           <Button type="button" variant="link">
             <ArrowLeftIcon /> Back
