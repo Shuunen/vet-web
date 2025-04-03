913fe38ea4032ca32326a6a7b4c834126c557de1
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-04-03 15:10:56 +0200

    feat: make className prop optional in SourceCode component

--- src/components/atoms/source-code.tsx
+++ src/components/atoms/source-code.tsx
@@ -1,7 +1,7 @@
 import { spaceIndent } from '@/utils/styling.const'
 import { cn } from '@/utils/styling.utils'
 
-export function SourceCode({ className, code }: { className: string; code: string | object }) {
+export function SourceCode({ className, code }: { className?: string; code: string | object }) {
   const json = typeof code === 'string' ? code : JSON.stringify(code, null, spaceIndent)
   return (
     <pre className={cn('bg-stone-100 p-6 border-stone-300 border rounded-lg', className)}>
