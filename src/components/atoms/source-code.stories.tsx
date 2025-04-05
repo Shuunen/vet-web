044c5e75f6c21e080f4548a84096c4ea0f8c9327
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-04-05 14:36:26 +0200

    feat: add storybook stories for SourceCode component

---
+++ src/components/atoms/source-code.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { SourceCode } from './source-code'
+
+const meta = {
+  component: SourceCode,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  title: 'atoms/Source code',
+} satisfies Meta<typeof SourceCode>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const StringCode: Story = {
+  args: {
+    code: 'const example = "Hello, World!";',
+  },
+}
+
+export const ObjectCode: Story = {
+  args: {
+    code: { key: 'value', nested: { anotherKey: 'anotherValue' } },
+  },
+}
