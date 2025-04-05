2c8c3f68f5c6c538abfc1b131532e8ac048f0002
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-04-05 15:05:08 +0200

    feat: add Storybook TanStack router handling

---
+++ .storybook/preview.tsx
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react'
+import { RouterProvider, createMemoryHistory, createRootRoute, createRouter } from '@tanstack/react-router'
+import '../src/index.css'
+
+const preview: Preview = {
+  decorators: [
+    Story => {
+      return (
+        <RouterProvider
+          router={createRouter({
+            history: createMemoryHistory(),
+            routeTree: createRootRoute({
+              component: Story,
+            }),
+          })}
+        />
+      )
+    },
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
