9e92b672ae40b6d55b029fda4e246919ef6328e5
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-06-13 13:38:18 +0200

    fix: update TanStackRouter import and restructure Vite config for better build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
+// oxlint-disable-next-line no-anonymous-default-export, no-default-export
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react')) return 'react'
+          if (id.includes('node_modules')) return 'vendor'
+        },
+      },
+    },
+  },
+  plugins: [tanstackRouter({ autoCodeSplitting: true, target: 'react' }), react(), tailwindcss()],
   resolve: {
     alias: {
+      // oxlint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src'),
     },
   },
+
 })
