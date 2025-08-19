a3b3e6c4a5ae64fa2acb5a439b00eb38d0f1a2f4
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-08-19 15:40:46 +0200

    Revert "chore: use up to date vitest/storybook config setup"
    This reverts commit 753233a74b667a7fa33d0bd2bb969373baf3ed2c.

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,7 +1,9 @@
-import { setProjectAnnotations } from '@storybook/react-vite'
-// oxlint-disable-next-line no-namespace
-import * as previewAnnotations from './preview.tsx'
+import { beforeAll } from 'vitest';
+import { setProjectAnnotations } from '@storybook/react-vite';
+import * as projectAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([previewAnnotations])
+const project = setProjectAnnotations([projectAnnotations]);
+
+beforeAll(project.beforeAll);
